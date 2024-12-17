b0b0e1540a30c46aab9ebbe7b11559ab7b362bf0
edward <edhu18osb@gmail.com>
2024-12-17 10:07:42 -0500

    validatev2.00

---
+++ tailwind.config.mjs
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {colors:{
+			// primary:"#7605f9c7",
+			
+			// secundari:"#fedf4c",
+			// terciario:"#7605f9"
+			primary:"#ee3c5f",
+			
+			secundari:"#fedf4c",
+			terciario:"#7605f9",
+			titulo:"#48255a"
+		}},
+	},
+	plugins: [],
+}
